dc3625443fa69336d33578bd10037ef69ca81bac
mini <mini13.1994@gmail.com>
2021-01-07 17:55:05 +0530

    commit on 7th jan

---
+++ api/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from api.serializers import BookSerializer
+from books.models import Book
+from django.http import Http404
+
+# Create your views here.
+
+# api/books -> for creating and listing all books
+
+class Books(APIView):
+    def get(self,request):
+        books=Book.objects.all()
+        serializer=BookSerializer(books,many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer=BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#api/books/1
+
+class BookDetails(APIView):
+    def get_object(self,pk):
+        try:
+            return Book.objects.get(id=pk)
+        except:
+            raise Http404
+    def get(self,request,pk):
+        book=self.get_object(pk)
+        serializer=BookSerializer(book)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def put(self,request,pk,format=None):
+        book=self.get_object(pk)
+        serializer = BookSerializer(instance=book,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self,request,pk,format=None):
+        book = self.get_object(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
---
+++ books/forms.py
@@ -0,0 +1,8 @@
+from books.models import Book
+from django.forms import ModelForm
+
+class BookCreateForm(ModelForm):
+    class Meta:
+        model=Book
+        fields="__all__"
+
